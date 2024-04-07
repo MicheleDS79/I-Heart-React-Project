9f917af9bf27b2ecc1ee6f1ab670d6441d21c076
Mike Sherokee <michael.sherokee@gmail.com>
2024-04-07 00:43:43 -0400

    Third Commit I Heart React Project

---
+++ src/components/CandyColoredHaeart.js
@@ -0,0 +1,19 @@
+import HeartSVG from "./HeartSVG";
+
+const colors = [
+    '#b8e3f5',
+    '#f5b8e4',
+    '#b8f5b9',
+    '#f5f3b8',
+    '#e3b8f5',
+    '#fffcf2'
+];
+
+const CandyColoredHeart = () => {
+    const randomCol = colors[Math.floor(Math.random() * colors.length)];
+    return(
+        <div className='heart'/>
+        <div className='heart-img'/>
+
+    )
+};
